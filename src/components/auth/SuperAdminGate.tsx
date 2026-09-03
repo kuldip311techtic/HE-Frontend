1b11f022f34071770e7264cf54fddd0c5a340050
techtic-mahir12 <mahir@techtic.agency>
2026-09-03 12:46:23 +0530

    clear

--- src/components/auth/SuperAdminGate.tsx
+++
@@ -1,7 +0,0 @@
-import { RoleGate } from "@/components/auth/RoleGate";
-
-export function SuperAdminGate({ children }: { children: React.ReactNode }) {
-  return (
-    <RoleGate allowedRoles={["super_admin"]}>{children}</RoleGate>
-  );
-}
