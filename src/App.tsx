a9938bcb094da1978c8436429c0c8b0ecf00cecb
techtic-mahir12 <mahir@techtic.agency>
2026-09-03 16:21:37 +0530

    fix(frontend): address Luna live validation findings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { AdminRoutes } from '@/routes/admin-routes';
-import { AdminApiBootstrap } from '@/components/bootstrap/AdminApiBootstrap';
 
 export default function App() {
-  return (
-    <>
-      <AdminApiBootstrap />
-      <AdminRoutes />
-    </>
-  );
+  return <AdminRoutes />;
 }
